e07ba35febb790c356404bd3d3f070da3e0df70f
Vandana-U <uvandana0502@gmail.com>
2024-05-20 14:23:29 +0530

    Added DemoDriver class

---
+++ DemoDriver.java
@@ -0,0 +1,10 @@
+public class DemoDriver{
+public static void main(String[]args){
+System.out.println("Enter the 2 numbers to print sum");
+Scanner sc=new Scanner(System.in);
+int n1=sc.nextInt();
+int n2=sc.nextInt();
+Demo d=new DemoImpl();
+d.sub(n1,n2);
+}
+}
